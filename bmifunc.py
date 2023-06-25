1df9b0f5fc3b2daf50d3a6a3551e3c5f2b15841c
SilimHussein <silimhussein16@gmail.com>
2023-06-25 12:45:21 +0300

    bmi, hello world

---
+++ bmifunc.py
@@ -0,0 +1,5 @@
+def bmi (weight, height):
+    bmi = weight / (height * height)
+    return bmi
+
+print (bmi(34, 162))
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+age= int(input("how old are you: "))
+print(age)
+
+if age > 18:
+    print("you are an adult")
+else:
+    print("you are a child")
+
+def bmi (weight, height):
+    bmi = weight / (height * height)
+    return bmi
+    
